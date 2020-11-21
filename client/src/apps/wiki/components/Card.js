033afad610229161403d0b409a8eec18e784343f
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-11-21 16:17:05 +0700

    update

--- client/src/apps/wiki/components/Card.js
+++ client/src/apps/wiki/components/Card.js
@@ -1,8 +1,22 @@
-import React from 'react'
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react'
 
-const Card = ({ title, text, pageid }) => {
+const Card = ({ title, text, pageid, special }) => {
   return (
-    <div className="card">
+    <div
+      className="card"
+      css={css`
+        .card-body {
+          .card-title {
+            color: var(--bs-primary);
+            &:hover {
+              color: var(--bs-success);
+            }
+          }
+        }
+      `}
+    >
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p
--- client/src/apps/wiki/components/CardList.js
+++ client/src/apps/wiki/components/CardList.js
@@ -2,11 +2,17 @@ import React from 'react'
 import Card from './Card'
 
 const CardList = ({ results }) => {
+  console.log(results)
   return (
     <div className="row mt-0 g-3">
       {results.map((v) => (
         <div className="col-md-4" key={v.pageid}>
-          <Card title={v.title} text={v.snippet} pageid={v.pageid} />
+          <Card
+            special={v.pageid % 2 === 0}
+            title={v.title}
+            text={v.snippet}
+            pageid={v.pageid}
+          />
         </div>
       ))}
     </div>
