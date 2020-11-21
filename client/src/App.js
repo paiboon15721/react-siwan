1065328201e62ccdff03fd8938b44f8eeb5cce25
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-11-21 11:09:42 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const name = 'Paiboon'
+const comments = ['comment1', 'comment2', 'comment3', 'comment4']
 
 const App = () => {
+  // const [comments, setComments] = useState([])
+
+  const onSubmit = (e) => {
+    console.log('Submit')
+  }
+
   return (
     <div className="container">
-      <h1>Hello, {name}</h1>
+      <form className="mt-5" onSubmit={onSubmit}>
+        <label className="form-label">Comment Input</label>
+        <input type="text" className="form-control" />
+      </form>
+      {comments.map((v) => (
+        <div className="card">
+          <div className="card-body">{v}</div>
+        </div>
+      ))}
     </div>
   )
 }
---
+++ client/src/map.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3]
+
+const results = numbers.map((v) => v * 2)
+
+console.log(results)
