7f57b409d21d5bc92b507b69d6c86f43ffc2d645
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-11-21 11:24:19 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
 
-const comments = ['comment1', 'comment2', 'comment3', 'comment4']
-
 const App = () => {
-  // const [comments, setComments] = useState([])
+  const [comments, setComments] = useState([])
+  const [currentComment, setCurrentComment] = useState('')
 
   const onSubmit = (e) => {
+    e.preventDefault()
     console.log('Submit')
+  }
+
+  const onCommentChange = (e) => {
+    console.log(e.target.value)
   }
 
   return (
     <div className="container">
       <form className="mt-5" onSubmit={onSubmit}>
         <label className="form-label">Comment Input</label>
-        <input type="text" className="form-control" />
+        <input
+          type="text"
+          className="form-control"
+          onChange={onCommentChange}
+        />
       </form>
       {comments.map((v) => (
         <div className="card">
