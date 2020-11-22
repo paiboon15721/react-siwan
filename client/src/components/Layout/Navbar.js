80c9678386cc8f8c0b8891f671b52014258029eb
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-11-22 11:04:34 +0700

    update

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,34 +1,47 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
-const Navbar = () => (
-  <nav
-    id="sidebarMenu"
-    className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
-  >
-    <div className="position-sticky pt-3">
-      <ul className="nav flex-column">
-        <li className="nav-item">
-          <Link className="nav-link active" to="/table">
-            <span data-feather="home"></span>
-            Table
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/wiki">
-            <span data-feather="home"></span>
-            Wiki
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/comments">
-            <span data-feather="home"></span>
-            Comments
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
+const menus = [
+  {
+    path: 'table',
+  },
+  {
+    path: 'wiki',
+  },
+  {
+    path: 'comments',
+  },
+  {
+    path: 'controlled-form',
+  },
+]
+
+const Navbar = () => {
+  const { pathname } = useLocation()
+
+  return (
+    <nav
+      id="sidebarMenu"
+      className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+    >
+      <div className="position-sticky pt-3">
+        <ul className="nav flex-column">
+          {menus.map((v) => (
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${
+                  pathname === `/${v.path}` ? 'active' : ''
+                }`}
+                to={`/${v.path}`}
+              >
+                {v.path}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
 
 export default Navbar
--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -2,27 +2,42 @@ import React from 'react'
 import Table from '../pages/Table'
 import WikiApp from '../apps/wiki/App'
 import CommentsApp from '../apps/comments/App'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 import Layout from '../components/Layout'
 import Login from '../pages/Login'
 
 const MyRouter = () => (
   <Router>
     <Switch>
+      <Route exact path="/">
+        <Redirect to="/table" />
+      </Route>
       <Route path="/login">
         <Login />
       </Route>
-      <Layout>
-        <Route path="/table">
+      <Route path="/table">
+        <Layout>
           <Table />
-        </Route>
-        <Route path="/wiki">
+        </Layout>
+      </Route>
+      <Route path="/wiki">
+        <Layout>
           <WikiApp />
-        </Route>
-        <Route path="/comments">
+        </Layout>
+      </Route>
+      <Route path="/comments">
+        <Layout>
           <CommentsApp />
-        </Route>
-      </Layout>
+        </Layout>
+      </Route>
+      <Route exact path="*">
+        <h1>404 Not Found</h1>
+      </Route>
     </Switch>
   </Router>
 )
