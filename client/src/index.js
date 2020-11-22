c683a240b049053ce2ff728278d2e27faa1050b1
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-11-22 09:57:03 +0700

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './scss/custom.scss'
-import App from './apps/wiki/App'
+import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
