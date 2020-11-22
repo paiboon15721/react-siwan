0d3b3d4e7dd258704ef66b4e2a39881358eee1a0
Paiboon Auengkongkatong <paiboon@clusterkit.co.th>
2020-11-22 11:34:50 +0700

    update

--- client/src/pages/ControlledForm.js
+++ client/src/pages/ControlledForm.js
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const ControlledForm = () => {
-  return <h1>My Form</h1>
+  const [name, setName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [department, setDepartment] = useState('3')
+
+  useEffect(() => {
+    console.log('Did mount')
+  }, [])
+
+  useEffect(() => {
+    console.log('Change when typing name')
+  }, [name, department])
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log({
+      name,
+      lastName,
+      department,
+    })
+    // Get form data
+    // Send data to API
+  }
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="mb-3 row">
+        <label className="col-sm-2 col-form-label">Name</label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="form-control"
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label className="col-sm-2 col-form-label">LastName</label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            className="form-control"
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label className="col-sm-2 col-form-label">Department</label>
+        <div className="col-sm-10">
+          <select
+            value={department}
+            onChange={(e) => setDepartment(e.target.value)}
+            className="form-select"
+            aria-label="Default select example"
+          >
+            <option value="1">Technical</option>
+            <option value="2">Accounting</option>
+            <option value="3">Manager</option>
+          </select>
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+    </form>
+  )
 }
 
 export default ControlledForm
